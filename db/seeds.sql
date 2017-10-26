1d390135fbfd13cba4239a678cd86ee9380ae760
Vignesh Balakrishnan <vigneshbalakrishnan@Vik.local>
2017-10-26 16:58:02 -0700

    eatburgerhw

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+###seeds
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Choclate Frog', false); 
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bertie Botts Every Flavor Bean', false);
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Pumpkin Pastry', false);
